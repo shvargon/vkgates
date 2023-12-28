f674b705bb9dfd99f9cc43b6f2837ba21fcffe1f
Valery Shvargonov <animekazna@gmail.com>
2023-12-28 13:27:13 +0700

    refactoring send photos http

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod config;
 mod deserialize_callback;
+
 use actix_web::{
     error, get,
     web::Json,
@@ -7,13 +8,13 @@ use actix_web::{
     App, HttpResponse, HttpServer, Responder,
 };
 use config::*;
-
 use deserialize_callback::attachments::photo::PhotoItems;
 use deserialize_callback::*;
-
 use teloxide::types::{InputFile, InputMedia};
 use teloxide::{prelude::*, types::InputMediaPhoto};
 use url::Url;
+
+use crate::deserialize_callback::attachments::photo::PhotoSizes;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -22,109 +23,88 @@ async fn hello() -> impl Responder {
 
 async fn index(req: Json<RequestData>, state: Data<AppState>) -> impl Responder {
     // @TODO check secret key equal
+    async fn bot_send_text(bot: &Bot, group_id: String, text: String) {
+        let sendmsg = bot.send_message(group_id, text).await;
 
-    match req.into_inner() {
-        RequestData::Confirmation(val) => {
-            dbg!("Respond confirmation", val);
-            HttpResponse::Ok().body(state.vk_confirmation_token.clone())
+        match sendmsg {
+            Ok(_) => println!("msg send"),
+            Err(err) => println!("#{:?}", err),
         }
-        RequestData::WallPostNew(val) => {
-            dbg!("Respond message", &val);
-            // @todo handle error
-
-            let text = &val.text;
-            let text = format!(
-                "{} https://vk.com/wall-{}_{}",
-                text, state.vk_community_id, val.id
-            );
-
-            if let Some(atachments) = &val.attachments {
-                // let mut
-
-                let mut photos: Vec<InputMedia> = vec![];
-                for current in atachments {
-                    match current {
-                        Attachments::Photo(values) => {
-                            if let Some(max) = PhotoItems::max_proportional_image(values) {
-                                let url = Url::parse(&max.url);
-                                if let Ok(url) = url {
-                                    let media = InputFile::url(url);
-                                    let caption: Option<String> = if photos.len() == 0 {
-                                        Some(text.clone())
-                                    } else {
-                                        None
-                                    };
-
-                                    let media = InputMediaPhoto {
-                                        media: media,
-                                        has_spoiler: false,
-                                        caption: caption,
-                                        parse_mode: None,
-                                        caption_entities: None,
-                                    };
-                                    let media: InputMedia = InputMedia::Photo(media);
-                                    photos.push(media);
-                                }
-                                // let url = InputFile::url(max.url)
-                            }
-                        }
-                        _ => {}
-                    }
-                }
-
-                let bot = &state.bot;
-
-                let _ = if photos.len() == 0 {
-                    bot.send_message(state.telegram_group_id.clone(), &text)
-                        .await;
-                } else {
-                    bot.send_media_group(state.telegram_group_id.clone(), photos)
-                        .await;
-                };
-            } else {
-                let sendmsg = state
-                    .bot
-                    .send_message(state.telegram_group_id.clone(), &text)
-                    .await;
-
-                match sendmsg {
-                    Ok(_) => {
-                        println!("msg send")
-                    }
-                    Err(err) => {
-                        println!("#{:?}", err)
-                    }
-                }
-            }
-
-            HttpResponse::Ok().body("ok")
-        }
-        _ => HttpResponse::Ok().body("ok"),
     }
+
+    let req = req.into_inner();
+
+    if let RequestData::Confirmation(_) = req {
+        return HttpResponse::Ok().body(state.vk_confirmation_token.clone());
+    }
+
+    if let RequestData::WallPostNew(val) = req {
+        dbg!("Respond message", &val);
+
+        let bot = &state.bot;
+        let group_id: String = state.telegram_group_id.clone();
+        let text = format!(
+            "{} https://vk.com/wall-{}_{}",
+            &val.text, state.vk_community_id, val.id
+        );
+
+        let photos = val.attachments.unwrap_or(vec![]);
+
+        let photos: Vec<&PhotoItems> = photos
+            .iter()
+            .filter_map(|val| match val {
+                Attachments::Photo(attachments) => Some(attachments),
+                _ => None,
+            })
+            .collect();
+
+        let photos: Vec<&PhotoSizes> = photos
+            .iter()
+            .filter_map(|val| PhotoItems::max_proportional_image(val))
+            .collect();
+
+        let photos: Vec<InputMedia> = photos
+            .iter()
+            .filter_map(|val| match Url::parse(val.url.as_str()) {
+                Ok(url) => {
+                    let media = InputMediaPhoto {
+                        media: InputFile::url(url),
+                        caption: None,
+                        has_spoiler: false,
+                        parse_mode: None,
+                        caption_entities: None,
+                    };
+                    Some(InputMedia::Photo(media))
+                }
+                Err(_) => None,
+            })
+            .collect();
+
+        if photos.len() == 0 {
+            bot_send_text(bot, group_id, text).await;
+        } else {
+            bot.send_media_group(group_id.clone(), photos).await;
+        }
+    }
+
+    HttpResponse::Ok().body("ok")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // @TODO thread spawn?
-    
+
     let bot = Bot::from_env();
     let state = Data::new(config::read_config(bot));
-
-    // bot.send_message(groupid, "hello world").await;
+    let json_config = web::JsonConfig::default().error_handler(|err, _req| {
+        dbg!(&err);
+        error::InternalError::from_response(err, HttpResponse::Conflict().finish()).into()
+    });
 
     HttpServer::new(move || {
-        let json_config = web::JsonConfig::default()
-            // .limit(904096)
-            .error_handler(|err, _req| {
-                dbg!(&err);
-                // create custom error response
-                error::InternalError::from_response(err, HttpResponse::Conflict().finish()).into()
-            });
-
         App::new().service(hello).service(
             web::resource("/")
-                // change json extractor configuration
-                .app_data(json_config)
+                .app_data(json_config.clone())
                 .app_data(state.clone())
                 .route(web::post().to(index)),
         )
