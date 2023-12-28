0d6970cb8caa2379b52021df5803d805e9b6cbb2
Valery Shvargonov <animekazna@gmail.com>
2023-12-28 21:15:09 +0700

    remove community id

--- src/config.rs
+++ src/config.rs
@@ -9,8 +9,6 @@ struct Cli {
     #[clap(long, env)]
     vk_confirmation_token: String,
     #[clap(long, env)]
-    vk_community_id: String,
-    #[clap(long, env)]
     vk_secret: Option<String>,
     #[clap(long, env)]
     host: Option<String>,
@@ -21,7 +19,7 @@ struct Cli {
 #[derive(Debug, Clone)]
 pub struct AppState {
     pub vk_confirmation_token: String,
-    pub vk_community_id: String,
+
     pub vk_secret: Option<String>,
     // vkcommunityid: u32,
     pub bot: Bot,
@@ -37,7 +35,6 @@ pub fn read_config() -> (Option<String>, Option<u16>, AppState) {
 
     let state = AppState {
         vk_confirmation_token: cli.vk_confirmation_token,
-        vk_community_id: cli.vk_community_id,
         vk_secret: cli.vk_secret,
         bot: bot,
         telegram_group_id: groupid,
